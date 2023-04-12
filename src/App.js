84e39b08923ae11c18f8964813f343d838860ee4
Faris Chew <farischew@gmail.com>
2023-04-12 18:25:11 +0800

    Adds form and NewExpense to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -28,7 +29,12 @@ function App() {
     },
   ];
 
-  return <Expenses expenses={expenses} />;
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />;
+    </div>
+  );
 }
 
 export default App;
