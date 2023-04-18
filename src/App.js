e778296d67a7ee7d5aa3be1c8f9397a2dc85c761
Faris Chew <farischew@gmail.com>
2023-04-18 18:42:48 +0800

    lifted up expensedata state

--- src/App.js
+++ src/App.js
@@ -29,9 +29,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("in App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />;
     </div>
   );
