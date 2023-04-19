c0b03ff02814045fbdbb333b6779f4356d96ffb3
Faris Chew <farischew@gmail.com>
2023-04-19 16:35:21 +0800

    Adds dynamic list in Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,36 +13,21 @@ const Expenses = (props) => {
   };
 
   return (
-    <Card className="expenses">
-      <div>
+    <div>
+      <Card className="expenses">
         <ExpensesFilter
           selected={expenseYear}
           onExpenseFilter={selectExpenseYearHandler}
         />
-      </div>
-      <div>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
-      </div>
-    </Card>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
